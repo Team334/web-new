be903e5b82e05b429dc035556ba9ff39c50657c9
Jerry <zeruiz@nycstudents.net>
2024-03-03 17:13:05 -0500

    [Needs Improvement] Use old layout

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,24 +1,43 @@
 import Container from "../_components/container";
 import { HeroPost } from "@/app/_components/hero-post";
 import { getAllPosts } from "@/components/markdown/api";
+import {Link} from "@nextui-org/link";
+import {Image} from "@nextui-org/react";
 
 export default function BlogPage() {
-    const allPosts = getAllPosts();
-    const heroPost = allPosts[0];
+    const posts = getAllPosts();
 
 
     return (
-        <div>
-            <Container>
-                <HeroPost
-                    title={heroPost.title}
-                    coverImage={heroPost.coverImage}
-                    date={heroPost.date}
-                    author={heroPost.author}
-                    slug={heroPost.slug}
-                    excerpt={heroPost.excerpt}
-                />
-            </Container>
+        <div className="">
+            <div className="grid grid-cols-2 gap-4">
+                {posts
+                    .sort((a, b) => new Date(a.date) < new Date(b.date) ? 1 : -1)
+                    .map((post) => (
+                        <Link href={`/posts/${post.slug}`} key={post.slug}>
+                            <div
+                                className="rounded-[1.5rem] overflow-hidden bg-slate-900 cursor-pointer duration-300 ease-out hover:-translate-y-2">
+                                <div className="relative h-40 w-full">
+                                    {post.coverImage && (
+                                        <Image
+                                            alt={"blogImage"}
+                                            src={`${post.coverImage}`}
+                                        />
+                                    )}
+                                    <div
+                                        className="relative w-full h-full flex bg-gradient-to-b from-transparent via-slate-900/70 to-slate-900 px-16 py-10"/>
+                                </div>
+                                <div className="p-5 pb-7">
+                                    <h1>{post.title} <span
+                                        className="text-xl font-normal text-slate-400">{new Date(post.date).toLocaleDateString()}</span>
+                                    </h1>
+                                    <p className="text-slate-400">{post.excerpt}</p>
+                                </div>
+                            </div>
+                        </Link>
+                    ))
+                }
+            </div>
         </div>
     );
 }
--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -2,12 +2,18 @@ import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getAllPosts, getPostBySlug } from "@/components/markdown/api";
 import markdownToHtml from "@/components/markdown/markdownToHTML";
-import Alert from "@/app/_components/alert";
-import Container from "@/app/_components/container";
-import Header from "@/app/_components/header";
-import { PostBody } from "../../_components/post-body";
-import { PostHeader } from "@/app/_components/post-header";
+// import Alert from "@/app/_components/alert";
+// import Container from "@/app/_components/container";
+// import Header from "@/app/_components/header";
+// import { PostBody } from "../../_components/post-body";
+// import { PostHeader } from "@/app/_components/post-header";
+// import coverImage from "@/app/_components/cover-image";
+import { format } from 'date-fns';
+import React from "react";
+import {Image} from "@nextui-org/react";
+import markdownStyles from "@/app/_components/markdown-styles.module.css";
 
+console.log("Hello world")
 
 export default async function Post({ params }: Params) {
     const post = getPostBySlug(params.slug);
@@ -21,21 +27,44 @@ export default async function Post({ params }: Params) {
     const content = await markdownToHtml(post.content || "");
 
     return (
-        <main>
-            <Alert preview={post.preview} />
-            <Container>
-                <Header />
-                <article className="mb-32">
-                    <PostHeader
-                        title={post.title}
-                        coverImage={post.coverImage}
-                        date={post.date}
-                        author={post.author}
-                    />
-                    <PostBody content={content} />
-                </article>
-            </Container>
-        </main>
+        // <main>
+        //     <Alert preview={post.preview} />
+        //     <Container>
+        //         <Header />
+        //         <article className="mb-32">
+        //             <PostHeader
+        //                 title={post.title}
+        //                 coverImage={post.coverImage}
+        //                 date={post.date}
+        //                 author={post.author}
+        //             />
+        //             <PostBody content={content} />
+        //         </article>
+        //     </Container>
+        // </main>
+
+    <div className="p-10">
+        <div className="relative h-72 w-full rounded-[1.5rem] overflow-hidden mb-10">
+            <Image
+                alt={"blogImage"}
+                src={post.coverImage}
+                className={"object-fill"}
+            />
+        </div>
+        <div
+            className="relative w-full h-full flex bg-gradient-to-b from-transparent via-slate-900/70 to-slate-900 px-16 py-10">
+            <div className="self-end">
+                <h1 className="text-5xl">{post.title}</h1>
+                <h4 className="font-normal">by {post.author.name} — {format(new Date(post.date), 'MMMM dd, yyyy')}</h4>
+            </div>
+        </div>
+        <article className="prose max-w-none dark:prose-invert">
+            <div
+                className={markdownStyles["markdown"]}
+                dangerouslySetInnerHTML={{ __html: content }}
+            />
+        </article>
+    </div>
     );
 }
 
@@ -45,7 +74,7 @@ type Params = {
     };
 };
 
-export function generateMetadata({ params }: Params): Metadata {
+export function generateMetadata({params}: Params): Metadata {
     const post = getPostBySlug(params.slug);
 
     if (!post) {
