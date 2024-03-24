638ec7e3f7910ac41debb04b541cbbed7625c7f8
Jerry <zeruiz@nycstudents.net>
2024-03-23 22:27:20 -0400

    Improve 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,17 @@
+import {Boxes} from "@/components/aceternity/ui/boxes";
+import {cn} from "@/components/cn"
+import React from "react";
+
 export default function NotFound() {
+
+
     return (
         <div
-            className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-            <div
-                className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-            <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-                404 | Not Found
-            </p>
+            className="h-[50rem] w-full overflow-hidden dark:bg-black bg-white relative flex flex-col items-center justify-center rounded-lg space-y-3">
+            <Boxes/>
+            <h1 className={cn("md:text-7xl text-4xl text-white relative z-20 main flex flex-row")}>
+                404 Not Found
+            </h1>
         </div>
     );
 }
