cbeb4ecf562096f3e58b88cb4807fabd684a7fd0
Jerry <zeruiz@nycstudents.net>
2024-04-11 17:47:04 -0400

    Redesign sponsors page

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -1,95 +1,70 @@
 "use client"
 import {AuroraBackground} from "@/components/aceternity/ui/aurora";
 import {motion} from "framer-motion";
-import {BentoGrid} from "@/components/aceternity/ui/bento-grid";
-import {BentoGridItem} from "@/components/aceternity/ui/direction-aware-hover";
+import {Image} from "@nextui-org/react";
 import React from "react";
+import {BackgroundGradient} from "@/components/aceternity/ui/bg-gradient-card";
+import {Link} from "@nextui-org/link";
 
 
 export default function SponsorPage() {
     const bentoItems = [
         {
             image: "/logos/first.png",
-            text: <div>
-                <h1 className={"main"}>FIRST</h1>
-            </div>,
+            title: "FIRST",
             url: "https://www.firstinspires.org/",
-            className:
-                "md:col-span-1",
+            description: ""
         },
         {
             image: "/sponsors/alumni-foundation.png",
-            text: <div>
-                <h1 className={"main"}>Brooklyn Tech Alumni Foundation</h1>
-            </div>,
+            title: "Brooklyn Tech Alumni Foundation",
             url: "",
-            className:
-                "md:col-span-2",
+            description: ""
         },
         {
             image: "/sponsors/arament.png",
-            text: <div>
-                <h1 className={"main"}>Arament Research, Development and Engineering Center</h1>
-            </div>,
+            title: "Arament Research, Development and Engineering Center",
             url: "",
-            className:
-                "md:col-span-2",
+            description: ""
         },
         {
             image: "/sponsors/con-edison.png",
-            text: <div>
-                <h1 className={"main"}>Con Edison</h1>
-            </div>,
+            title: "Con Edison",
             url: "https://www.coned.com/en",
-            className:
-                "md:col-span-1",
+            description: ""
         },
         {
             image: "/sponsors/dodstem.png",
-            text: <div>
-                <h1 className={"main"}>DoD STEM</h1>
-            </div>,
+            title: "DoD STEM",
             url: "https://www.dodstem.us",
-            className:
-                "md:col-span-1",
+            description: ""
         },
         {
             image: "/sponsors/haas-foundation.png",
-            text: <div>
-                <h1 className={"main"}>Gene HAAS Foundation</h1>
-            </div>,
+            title: "Gene HAAS Foundation",
             url: "https://ghaasfoundation.org/content/ghf/en/home.html",
-            className:
-                "md:col-span-1",
+            description: ""
         },
         {
             image: "/sponsors/ike-heller.png",
-            text: <div>
-                <h1 className={"main"}>Ike Heller</h1>
-            </div>,
+            title: "Ike Heller",
             url: "",
-            className:
-                "md:col-span-1",
+            description: ""
         },
         {
             image: "/sponsors/quotebeam.png",
-            text: <div>
-                <h1 className={"main"}>QuoteBeam</h1>
-            </div>,
+            title: "QuoteBeam",
             url: "https://quotebeam.com",
-            className:
-                "md:col-span-1.5",
+            description: ""
         },
         {
             image: "/sponsors/whimsy-tech.png",
-            text: <div>
-                <h1 className={"main"}>Whimsy Tech</h1>
-            </div>,
+            title: "Whimsy Tech",
             url: "https://whimsytech.com",
-            className:
-                "md:col-span-2",
-        },
+            description: ""
+        }
     ];
+
 
     return (
         <div className={"w-full"}>
@@ -112,18 +87,35 @@ export default function SponsorPage() {
                     </div>
                 </motion.div>
             </AuroraBackground>
-            <div className={"h-full py-20"}>
-                <BentoGrid className="max-w-8xl mx-auto md:auto-rows-[30rem] p-10">
+            <div className={"h-full py-20 items-center justify-center"}>
+                <div className="flex flex-wrap justify-center gap-5">
                     {bentoItems.map((item, i) => (
-                        <BentoGridItem
-                            key={i}
-                            image={item.image}
-                            text={item.text}
-                            url={item.url}
-                            className={item.className}
-                        />
-                    ))}
-                </BentoGrid>
+                        <BackgroundGradient
+                            className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 flex flex-col justify-between"
+                            key={i}>
+                            <div className="h-[400px] w-[300px] flex flex-col justify-between">
+                                <div className="flex items-center justify-center h-[300px] overflow-hidden">
+                                    <Image
+                                        src={item.image}
+                                        alt="sponsor"
+                                        width={300}
+                                        height={300}
+                                        className="max-w-full max-h-full object-contain object-center"
+                                    />
+                                </div>
+                                <div className="mt-4">
+                                    <Link className="text-base sm:text-xl text-black dark:text-neutral-200"
+                                          href={item.url}>
+                                        {item.title}
+                                    </Link>
+                                    <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                                        {item.description}
+                                    </p>
+                                </div>
+                            </div>
+                        </BackgroundGradient>
+                        ))}
+                </div>
             </div>
         </div>
     );
