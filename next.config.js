4e9f18b60f75d0ab62a31da874f726016e4dd591
Jerry <zeruiz@nycstudents.net>
2024-04-13 21:30:03 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     modularizeImports: {
         "react-icons/?(((\\w*)?/?)*)": {
             transform: "@react-icons/all-files/{{ matches.[1] }}/{{ member }}",
