748b1398b65346ed4c090802433342d20c4469a5
Jerry <zeruiz@nycstudents.net>
2024-04-13 21:25:57 -0400

    Revert changes

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,15 +1,9 @@
 import {NextRequest, NextResponse} from "next/server";
-import {getPostBySlug, getAllPosts} from "../markdown";
+import {getPostBySlug} from "../markdown";
 
 
 export async function GET(request: NextRequest, {params}: { params: { slug: string } }) {
     const slug = params.slug;
     const post = getPostBySlug(slug);
     return NextResponse.json(post);
-}
-
-
-export async function generateStaticParams() {
-  const slugs = await getAllPosts();
-  return slugs.map((slug) => ({ slug }));
 }
