bec8a9326873c0d0d34e4facfdc495ea21fb9790
Jerry <zeruiz@nycstudents.net>
2024-04-16 21:50:30 -0400

    Progress

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,9 +1,17 @@
 import {NextRequest, NextResponse} from "next/server";
-import {getPostBySlug} from "../markdown";
+import {getAllPosts, getPostBySlug} from "../markdown";
 
 
 export async function GET(request: NextRequest, {params}: { params: { slug: string } }) {
     const slug = params.slug;
     const post = getPostBySlug(slug);
     return NextResponse.json(post);
+}
+
+export async function generateStaticParams() {
+    const posts = getAllPosts()
+
+    return posts.map((post) => ({
+        slug: post.slug,
+    }))
 }
